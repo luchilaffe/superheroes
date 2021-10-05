5d1f29472ee4768a5160bc13c534bfd569903d77
Carlos Raúl Lafferriere <luchi_laffe@yahoo.com>
2021-10-04 23:06:21 -0300

    Integration Test

---
+++ src/test/java/com/mindata/superheroes/integration/IntegrationTest.java
@@ -0,0 +1,44 @@
+package com.mindata.superheroes.integration;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import com.mindata.superheroes.dao.SuperHeroesRepository;
+import com.mindata.superheroes.utils.RestEndpoints;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-integrationtest.properties")
+class IntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    SuperHeroesRepository repository;
+
+
+    @Test
+    void whenTestSomeMethodsThenReturnOk() throws Exception {
+
+        /* Get All */
+        this.mockMvc.perform(get(RestEndpoints.GET_ALL)).andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty()).andReturn();
+
+        /* Get One */
+        this.mockMvc.perform(get(RestEndpoints.GET + "/1")).andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty()).andReturn();
+
+        /* Search by Name */
+        this.mockMvc.perform(get(RestEndpoints.SEARCH + "?name=Man")).andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty()).andReturn();
+    }
+
+}
