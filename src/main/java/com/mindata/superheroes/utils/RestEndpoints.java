580023630df111f2e77d88f09626d8cfd04a5897
Carlos Raúl Lafferriere <luchi_laffe@yahoo.com>
2021-10-10 01:54:09 -0300

    Add User's Endpoints

--- src/main/java/com/mindata/superheroes/utils/RestEndpoints.java
+++ src/main/java/com/mindata/superheroes/utils/RestEndpoints.java
@@ -5,11 +5,17 @@ import lombok.Builder;
 @Builder
 public class RestEndpoints {
 
-    private static final String ROOT = "/superheroes";
-    public static final String GET_ALL = ROOT;
-    public static final String GET = ROOT;
-    public static final String SEARCH = ROOT + "/search";
-    public static final String UPDATE = ROOT;
-    public static final String DELETE = ROOT;
+    private static final String SUPER_HEROES = "/superheroes";
+    public static final String GET_ALL = SUPER_HEROES;
+    public static final String GET = SUPER_HEROES;
+    public static final String SEARCH = SUPER_HEROES + "/search";
+    public static final String UPDATE = SUPER_HEROES;
+    public static final String DELETE = SUPER_HEROES;
+
+    private static final String USERS = "/users";
+    public static final String USER_ADD = USERS;
+    public static final String USER_AUTH = USERS + "/authenticate";
+
+    private RestEndpoints() {}
 
 }
